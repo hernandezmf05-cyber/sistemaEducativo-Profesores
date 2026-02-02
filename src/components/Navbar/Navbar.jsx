8f925c09482f0169f66ca40093497e769376e905
Martha Ines Lopez A <minslopez@gmail.com>
2026-02-02 12:01:33 -0500

    fix: mejora estética del logo más transparente

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar({ query, setQuery, setCurrentView, toggleDarkMode, isDark, curre
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <img src="/logo.png" alt="Sura company logo for teacher management system navigation header" className="logo" /> {/* Logo de Sura */}
+        <img src="/descarga%20(1).png" alt="Sura logo" className="logo" />
         <h2>Sistema de Gestión de Profesores</h2> {/* Título de la aplicación */}
       </div>
       <ul className="nav-menu"> {/* Lista de elementos de navegación */}
